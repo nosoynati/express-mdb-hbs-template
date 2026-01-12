4a13b149f2ee207924a992dbd8140eed83a00624
Natasha <shotaoouji@gmail>
2026-01-11 21:46:29 -0300

    fix: css routing

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
-import {fileURLToPath} from 'url'
+import fs from 'fs';
+import { fileURLToPath } from 'url'
 import { engine } from 'express-handlebars';
 
 import router from '../routes/home.router.js';
 import logger from '../middleware/logger.js';
 
-import session from  'express-session';
+import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import MongoStore from 'connect-mongo';
 
@@ -20,8 +21,10 @@ const SECRET = process.env.SECRET;
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
+const publicPath = path.join(__dirname, '../../public');
 
 export const startServer = async () => {
+
   app.use(
     session({
       secret: SECRET,
@@ -30,10 +33,10 @@ export const startServer = async () => {
       cookie: {
         maxAge: 60 * 60 * 1000,
         httpOnly: true
-      } 
+      }
     })
   );
-
+  app.use(express.static(path.join(__dirname, '../../public')))
   app.engine("handlebars", engine({
     defaultLayout: "main",
     layoutDir: path.join(__dirname, "../views/layouts")
@@ -43,9 +46,7 @@ export const startServer = async () => {
   app.set("views", path.join(__dirname, "../views"))
 
   app.use(logger)
-  app.use('/static', express.static(path.join(__dirname, 'public')))
   app.use("/", router)
-
 
   app.listen(PORT, () => {
     console.log(`Started server at http://localhost:${PORT}`)
